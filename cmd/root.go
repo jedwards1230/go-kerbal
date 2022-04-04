b76d2aafe6c0c4faec6b613c9f63347da5eb111f
Justin Edwards <justinedwards1230@gmail.com>
2022-04-04 02:28:22 -0400

    added fullscreen

--- cmd/root.go
+++ cmd/root.go
@@ -5,19 +5,27 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jedwards1230/go-kerbal/cmd/config"
 	"github.com/jedwards1230/go-kerbal/tui"
 )
 
 func Execute() {
-	//config.LoadConfig()
-	//cfg := config.GetConfig()
+	config.LoadConfig()
+	cfg := config.GetConfig()
 
 	m := tui.InitialModel()
-	p := tea.NewProgram(m)
+	var opts []tea.ProgramOption
+
+	// Always append alt screen program option.
+	opts = append(opts, tea.WithAltScreen())
+
+	// If mousewheel is enabled, append it to the program options.
+	if cfg.Settings.EnableMouseWheel {
+		opts = append(opts, tea.WithMouseAllMotion())
+	}
+	p := tea.NewProgram(m, opts...)
 	if err := p.Start(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
-
-	//registry.BuildRegistry()
 }
