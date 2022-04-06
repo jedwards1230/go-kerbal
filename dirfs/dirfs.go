63a23c38c20454e64ce731a84c91472a55a72c14
Justin Edwards <justinedwards1230@gmail.com>
2022-04-06 16:30:25 -0400

    added root dir finder

--- dirfs/dirfs.go
+++ dirfs/dirfs.go
@@ -3,7 +3,17 @@ package dirfs
 import (
 	"errors"
 	"os"
+	"path"
+	"path/filepath"
+	"runtime"
 )
+
+// useeful for keeping unit tests on track with created directories
+func RootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
+}
 
 // GetHomeDirectory returns the users home directory.
 func GetHomeDirectory() (string, error) {
